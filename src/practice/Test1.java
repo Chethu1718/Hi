dc3e0d98ef5116ce2c91b67540f7e390bf4811c7
Chethan <Chethan@Chethan>
2023-08-21 21:22:50 +0530

    1st Commit

---
+++ src/practice/Test1.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Test1 
+{
+	public static void main(String[] args)
+	{
+		Test1 t=new Test1();
+		Test1 t1=t;
+		String t2 = t1.toString();
+		System.out.println(t2);
+		
+		
+	}
+	
+}
